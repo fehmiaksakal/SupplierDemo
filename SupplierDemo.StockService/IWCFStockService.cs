0590f8d2227fc1861117483a340581d41d9b5ed6
fehmiaksakal <fehmiaksakal01@gmail.com>
2020-06-14 13:48:18 +0300

    WCF added

---
+++ SupplierDemo.StockService/IWCFStockService.cs
@@ -0,0 +1,18 @@
+﻿using SupplierDemo.Entites.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SupplierDemo.StockService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWCFStockService" in both code and config file together.
+    [ServiceContract]
+    public interface IWCFStockService
+    {
+        [OperationContract]
+        List<Stock> GetAllStock();
+    }
+}
---
+++ SupplierDemo.StockService/WCFStockService.cs
@@ -0,0 +1,27 @@
+﻿using SupplierDemo.Business.Abstract;
+using SupplierDemo.Entites.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SupplierDemo.StockService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFStockService" in both code and config file together.
+    public class WCFStockService : IWCFStockService
+    {
+        private IStockService _stockService;
+
+        public WCFStockService(IStockService stockService)
+        {
+            _stockService = stockService;
+        }
+
+        public List<Stock> GetAllStock()
+        {
+            return _stockService.GetAll();
+        }
+    }
+}
