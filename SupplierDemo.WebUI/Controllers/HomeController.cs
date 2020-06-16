d4ec061bbb0f822ec6e7d7c993abd12a7a1be92b
fehmiaksakal <fehmiaksakal01@gmail.com>
2020-06-16 22:28:04 +0300

    Added Backup XML

--- SupplierDemo.WebUI/Controllers/HomeController.cs
+++ SupplierDemo.WebUI/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.ServiceModel;
 using SupplierDemo.Entites.Entities;
 using SupplierDemo.StockServices.Model;
+using System.Xml.Serialization;
+using System.Xml.Linq;
 
 namespace SupplierDemo.WebUI.Controllers
 {
@@ -30,11 +32,20 @@ namespace SupplierDemo.WebUI.Controllers
         {
 
             List<StockVM> resp = service.GetOnlyStock();
-
-
+            XmlSerializer ser = new XmlSerializer(typeof(StockVM));
+            string curName = supId + " " + DateTime.Now.ToString().Replace(".","-").Replace(":", ";");
+            string savePath = Server.MapPath(@"/XMLBackUP/" + curName + ".xml");
+            var xmlSavePath = new XElement("Stocks",
+                    from st in resp
+                    select new XElement(
+                            "Stock",
+                                new XElement("StockId", st.StockId),
+                                new XElement("SupplierName", st.SupplierId),
+                                new XElement("ProductName", st.ProductId),
+                                new XElement("Quantity", st.Quatity)
+                        ));
+            xmlSavePath.Save(savePath);
             return resp;
         }
-
-
     }
 }
